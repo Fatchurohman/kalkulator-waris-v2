be886290ca16718e8139f899812925f86dacc1b2
Fatchurohman <fatur62@gmail.com>
2026-08-04 11:25:52 +0700

    Add service worker with caching functionality
    Implement service worker for caching resources

--- sw.js
+++ sw.js
@@ -1,1 +1,22 @@
+const CACHE_NAME = 'waris-syafii-v2-cache';
+const urlsToCache = [
+  './',
+  './index.html',
+  './script.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
 
